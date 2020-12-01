ac7f8f99f50ac789c1c358d995cd3d73bd9592c3
Matias <m.sepulveda35@ufromail.cl>
2020-12-01 11:06:15 -0300

    programa version 0.3

--- taller/src/main/java/Taller02UnitTesting.java
+++ taller/src/main/java/Taller02UnitTesting.java
@@ -9,22 +9,13 @@ public class Taller02UnitTesting {
         llenarMatriz(mat);
 
 
-       // mat[][] = generarMatriz(0,0);
-
-        /*verMatriz(mat, 24, dim2);
-        System.out.println("-------------");
-
-        llenarMatriz(mat, 100);
-
-        System.out.println("-------------");
-        verMatriz(mat, dim1, dim2);*/
     }
-    static int dias(int dias){
+
+    static int dias(int dias) {
         System.out.println("Ingrese los dias del mes");
-        dias = validadorEntrada(0,0);
+        dias = validadorEntrada(0, 0);
         return dias;
     }
-
 
 
     public static double[][] llenarMatriz(double[][] mat) {
@@ -40,45 +31,28 @@ public class Taller02UnitTesting {
 
     static int validadorEntrada(int n, int min) {
 
-            do {
-                Scanner teclado = new Scanner(System.in);
-                try {
-                    n = teclado.nextInt(); //numero que se validará
-                } catch (Exception e) {
-                    teclado.next();
-                    System.out.println("Ingrese un numero valido");
-                }
-                if (n < min) {
-                    System.out.println("ingrese un numero valido");
-                }
-            } while (n < min );
-            return n;
-        }
-    /*public static void sismoMayor(double[][] mat) {
-        int hora, dia;
-        double valor=0, valorMayor=0;
-
-        for (int i = 0; i < 24; i++) {
-            for (int j = 0; j < mat.length; j++) {
-                valor =  mat[i][j];
-                if (valor>valorMayor){
-                    valorMayor = valor;
-                    hora = i;
-                    dia = j;
-                }
-
+        do {
+            Scanner teclado = new Scanner(System.in);
+            try {
+                n = teclado.nextInt(); //numero que se validará
+            } catch (Exception e) {
+                teclado.next();
+                System.out.println("Ingrese un numero valido");
             }
-        }
-        System.out.println("El sismo mas alto: "+valorMayor+" en el dia: "+dia+" y en la hora: "+hora);
-    }*/
+            if (n < min) {
+                System.out.println("ingrese un numero valido");
+            }
+        } while (n < min);
+        return n;
+    }
 
-    public static void ordenarColumnas(double[][] mat,int dias){
+    public static void ordenarColumnas(double[][] mat, int dias) {
         double t;
-        for(int i=0; i < 24; i++){
-            for(int j=0;j< dias; j++){
-                for(int x=0; x < 24; x++){
-                    for(int y=0; y <dias; y++){
-                        if(mat[i][j] > mat[x][y]){
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < dias; j++) {
+                for (int x = 0; x < 24; x++) {
+                    for (int y = 0; y < dias; y++) {
+                        if (mat[i][j] > mat[x][y]) {
                             t = mat[i][j];
                             mat[i][j] = mat[x][y];
                             mat[x][y] = t;
@@ -88,36 +62,23 @@ public class Taller02UnitTesting {
             }
         }
     }
+}
 
-    public static void escalaSismica(double[][] mat, int dias){
+    /*public static void escalaSismica(double[][] mat, int dias){
+        boolean escalaS;
+        double valor1,valor2,valor3,valor4;
         for (int i = 0; i <24 ; i++) {
             for (int j = 0; j <dias; j++) {
-                if(){
+                valor1=mat[i][j];
 
-                }
+
             }
 
         }
 
 
+    }/*
+
+
     }
 
-    public int elegirOpcion() {
-        int opcion = 0;
-        Scanner teclado = new Scanner(System.in);
-        do {
-            try {
-                System.out.println("Ingrese opcion a revisar");
-                System.out.println("1.-¿Qué intensidad? ¿Qué día? y a ¿Qué hora ocurrió el sismo más intenso?");
-                System.out.println("2.-¿Cuántos días en total se registraron sismos ≥ 5.5 grados?");
-                System.out.println("3.-¿Hay alerta de escalada sísmica?.");
-                System.out.println("4.- Salir");
-                opcion = teclado.nextInt();
-            } catch (InputMismatchException e) {
-                teclado.next();
-                System.out.println("Dato ingresado erroneo");
-            }
-        } while (opcion < 1 || opcion > 3);
-        return opcion;
-    }
-}
